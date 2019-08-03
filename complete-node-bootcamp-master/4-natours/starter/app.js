3d937c90e17f1490975ec1cba3e9d841d51e20ba
Eliav <eliav.s.cohen@gmail.com>
2019-08-04 01:02:13 +0300

    04/08/2019 1:02

--- complete-node-bootcamp-master/4-natours/starter/app.js
+++ complete-node-bootcamp-master/4-natours/starter/app.js
@@ -1,1 +1,33 @@
-const express 
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const crud = require('./crud');
+const apiVersion = 1;
+app.use(express.json());
+
+// custom middleware
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 👋');
+    next();
+});
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+app.use(morgan());
+
+// Routes
+app.route(`/api/v${apiVersion}/tours`)
+    .get(crud.getAllTours)
+    .post(crud.createNewTour);
+
+app.route(`/api/v${apiVersion}/tours/:id`)
+    .get(crud.getTour)
+    .patch(crud.updateTour)
+    .delete(crud.deleteTour);
+
+// Server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+});
